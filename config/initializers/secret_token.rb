f5ac44c11f835c653c51f2f52b019d3d5f73fb8b
Tim Lee <timothy.t.lee@gmail.com>
2011-10-24 21:58:20 +0800

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Get2gether::Application.config.secret_token = 'aa60fbdb16ef2890cf5d78ab941595c5d82b840ad9bfaab74e8d43386331db5992be8c59084976c83e3256ed8528ad2178262c710ca8589c816712da8df9a828'
